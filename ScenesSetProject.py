734ace8a338f96756205782b03ed9e5da439201b
GastaP765 <gasta.psychic.bird@gmail.com>
2019-02-07 10:45:46 +0900

    Initial commit

---
+++ ScenesSetProject.py
@@ -0,0 +1,23 @@
+import maya.cmds as mc
+import maya.mel as mel
+
+def Open():
+	basicFilter = "All Files(*.*);;MayaFiles(*.ma *.mb);;Maya Ascii(*.ma);;Maya Binary(*.mb);;OBJ(*.obj);;FBX(*.fbx)"
+	path = mc.fileDialog2(ff=basicFilter, ds=1, fm=1, cap='open scenes')
+
+	a = path[0]
+	b = a.split('/scenes')
+
+	mel.eval('setProject "{}" ;'.format(b[0]))
+
+	fn = mc.file(q=True, sn=True)
+	if not fn:
+		mc.file(path[0], o=True, f=True)
+	else:
+		mc.file(s=True)
+		mc.file(path[0], o=True)
+
+def consoleKey():
+	Open()
+
+First()
